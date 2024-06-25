c809fd5ff521f19891cbc6ec308a9d852e031973
jhon-alex <japaricio1002@yahoo.com>
2024-06-25 16:57:12 -0500

    actualizacion de vite-app

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "normalize.css"
+
+
+import styled from 'styled-components'
+
+const FondoGradiente = styled.div`
+background: linear-gradient(175deg, #041833 4.16%, #04244F 48%, #154580 96.76);
+width: 100%;
+min-height: 100vh;
+`
+
+const App = () => {
+  return (
+    <FondoGradiente>
+    
+    </FondoGradiente>
+  );
+};
+
+
+export default App
